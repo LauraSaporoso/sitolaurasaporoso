be9792d5072c99664614ed604d16cc8c61c8ec59
LauraSaporoso <laurasaporoso99@gmail.com>
2022-12-09 18:00:19 +0100

    Javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+
+    var typed = new Typed(".animate", {
+      strings: [
+        "Hello World!",
+      ],
+      typeSpeed: 150,
+      backDelay: 100,
+      backSpeed: 80,
+      loop: true,
+    });
+  
